e2a024b12bd21e9c1c5ae1e00f54c5123ac031be
Shivaal Roy <shivaalroy@gmail.com>
2018-10-30 00:25:23 -0700

    include html and body tags when preparing email html

--- js/prepare_google_form.js
+++ js/prepare_google_form.js
@@ -3,8 +3,8 @@ function extractEmailHtml(input) {
     result = result.replace(/3D/g, "");
     result = result.replace(/\n\n/g, "\n");
 
-    const trim_start = result.indexOf("<table");
-    const trim_end = result.lastIndexOf("</body>");
+    const trim_start = result.indexOf("<html");
+    const trim_end = result.lastIndexOf("</html>") + "</html>".length;
     return result.substring(trim_start, trim_end);
 }
 
